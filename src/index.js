20fb5793275f25771d152e0d2b66308da38943ce
Gabriela Da Silva <gabriela@Gabrielas-MacBook-Pro.local>
2021-03-04 09:52:46 -0300

    estrutura basica

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Layout from './components/layout';
+import Home from './views/home'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
