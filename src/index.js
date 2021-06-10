6661781fe8aad74a9e186e98330cf3dfc904c3de
Kessyus Fófano <kessyus@gmail.com>
2021-06-09 22:04:45 -0300

    transfering my project to github

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import GlobalStyle from './assets/globalStyle';
-import { Provider } from 'react-redux';
-import store from './store';
-import Routers from './routers';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import GlobalStyle from "./assets/globalStyle";
+import { Provider } from "react-redux";
+import store from "./store";
+import Routers from "./routers";
 
 ReactDOM.render(
   <Provider store={store}>
     <GlobalStyle />
     <Routers />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
